5b336d6605102ec1667f0b17815f1cd0fd42d399
guchio33 <aokihikari3@gmail.com>
2022-11-08 17:05:02 +0900

    department,facultiesModelの作成

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_08_040005) do
+ActiveRecord::Schema.define(version: 2022_11_08_072412) do
+
+  create_table "faculties", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "provider", default: "email", null: false

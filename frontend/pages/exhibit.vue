abf65e2a7194044279b3d1708ac37a8c947b3a12
sayyyyyy <d958956a6b650@gmail.com>
2022-11-19 10:57:50 +0900

    exhibit画面レイアウトの追加

---
+++ frontend/pages/exhibit.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="exhibit-input-container">
+        <h1>商品詳細入力</h1>
+        <div class="exhibit-input-name-container">
+            <label for="exhibit-input-name">商品名</label>
+            <input type="text" id="exhibit-input-name">
+        </div>
+        <div class="exhibit-input-use-days-container">
+            <label for="exhibit-input-use-days">商品名</label>
+            <input type="number" id="exhibit-input-use-days">
+        </div>
+        <div class="exhibit-input-img-container">
+            <label for="exhibit-input-img">商品名</label>
+            <input type="file" id="exhibit-input-img">
+        </div>
+        <div class="exhibit-input-comment-container">
+            <label for="exhibit-input-comment">商品名</label>
+            <input type="file" id="exhibit-input-comment">
+        </div>
+        <button>出品する</button>
+        <!-- <p>{{ error_message }}</p> -->
+    </div>
+</template>

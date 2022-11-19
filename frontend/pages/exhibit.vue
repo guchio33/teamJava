916875f8d58a836e4be223464e98fd17a1253bf3
sayyyyyy <d958956a6b650@gmail.com>
2022-11-19 11:00:45 +0900

    exhibit表示内容微調整、正常に動作すること確認

--- frontend/pages/exhibit.vue
+++ frontend/pages/exhibit.vue
@@ -6,15 +6,15 @@
             <input type="text" id="exhibit-input-name">
         </div>
         <div class="exhibit-input-use-days-container">
-            <label for="exhibit-input-use-days">商品名</label>
+            <label for="exhibit-input-use-days">使用日数</label>
             <input type="number" id="exhibit-input-use-days">
         </div>
         <div class="exhibit-input-img-container">
-            <label for="exhibit-input-img">商品名</label>
+            <label for="exhibit-input-img">商品画像</label>
             <input type="file" id="exhibit-input-img">
         </div>
         <div class="exhibit-input-comment-container">
-            <label for="exhibit-input-comment">商品名</label>
+            <label for="exhibit-input-comment">コメント</label>
             <input type="file" id="exhibit-input-comment">
         </div>
         <button>出品する</button>

ef7a54bb81303742eaa4cc91edbf9a238215a568
sayyyyyy <d958956a6b650@gmail.com>
2022-11-19 10:41:31 +0900

    register画面機能作成、API以外

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -2,13 +2,15 @@
     <div class="login-input-container">
         <div class="login-input-mail-container">
             <label for="login-input-mail">e-mail</label>
-            <input id="login-input-mail" type="text">
+            <input id="login-input-mail" type="email" v-model="login_email">
         </div>
         <div class="login-input-password-container">
             <label for="login-input-password">パスワード</label>
-            <input id="login-input-password" type="text">
+            <input id="login-input-password" type="text" v-model="login_password">
         </div>
 
-        <button>新規登録</button>
+        <button v-on:click="loginUser">ログイン</button>
+        <p>{{ error_message }}</p>
     </div>
 </template>
+
--- frontend/pages/register.vue
+++ frontend/pages/register.vue
@@ -2,18 +2,68 @@
     <div class="register-input-container">
         <div class="register-input-name-container">
             <label for="register-input-name">名前</label>
-            <input id="register-input-name" type="text">
+            <input id="register-input-name" type="text" v-model="register_name">
         </div>
         <div class="register-input-mail-container">
             <label for="register-input-mail">e-mail</label>
-            <input id="register-input-mail" type="text">
+            <input id="register-input-mail" type="email" v-model="register_email">
         </div>
         <div class="register-input-password-container">
             <label for="register-input-password">パスワード</label>
-            <input id="register-input-password" type="text">
+            <input id="register-input-password" type="text" v-model="register_password">
         </div>
 
-        <button>新規登録</button>
+        <button v-on:click="registerUser()">新規登録</button>
+        <p>{{ error_message }}</p>
     </div>
 
 </template>
+
+<script setup lang="ts">
+const API_URL = 'http://localhost:4000'
+
+const register_name = ref('')
+const register_email = ref('')
+const register_password = ref('')
+let error_message = ref('')
+
+const registerUser = async () => {
+    if (!register_name.value) {
+        error_message.value = '名前を入力してください'
+        return
+    }
+
+    if (!register_email.value) {
+        error_message.value = 'メールアドレスを入力してください'
+        return
+    }
+
+    if (!register_password.value) {
+        error_message.value = 'パスワードを入力してください'
+        return
+    }
+    const register_data = {
+        'userName': register_name,
+        'email': register_email,
+        'password': register_password
+    }
+
+    //TODO: APIの呼び出し
+    try {
+        const registerUserController = await useFetch(API_URL, {
+            method: 'POST',
+            body: register_data
+        })
+
+        //TODO: レスポンスが返ってきたらユーザーデータの保存
+
+        navigateTo({path: '/top'})
+
+    } catch (e) {
+        console.log(e)
+        error_message.value = 'データの登録に失敗しました'
+        return
+    }
+
+}
+</script>

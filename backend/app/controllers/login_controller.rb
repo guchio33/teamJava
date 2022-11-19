bb8485766654163dc4f8e0d4df0c2244480e88bc
guchio33 <aokihikari3@gmail.com>
2022-11-19 18:25:10 +0900

    registerの追加

---
+++ backend/app/controllers/login_controller.rb
@@ -0,0 +1,10 @@
+class LoginController < ApplicationController
+    def index
+        # 
+        if current_user #deviseヘルパーメソッド
+            render json: {is_login: true, data:current_user}
+        else 
+            render json: {is_login: true, message: "ユーザーが存在しません"}
+        end 
+    end
+end

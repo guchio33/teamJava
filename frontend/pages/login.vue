d89193e0f4f7e4d6ec554377a1820ee437191f1b
sayyyyyy <d958956a6b650@gmail.com>
2022-11-19 10:05:26 +0900

    login画面レイアウトの追加

---
+++ frontend/pages/login.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="login-input-container">
+        <div class="login-input-mail-container">
+            <label for="login-input-mail">e-mail</label>
+            <input id="login-input-mail" type="text">
+        </div>
+        <div class="login-input-password-container">
+            <label for="login-input-password">パスワード</label>
+            <input id="login-input-password" type="text">
+        </div>
+
+        <button>新規登録</button>
+    </div>
+</template>

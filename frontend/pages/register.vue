6a93f8cce45020e3d674e1bed205f58269655857
sayyyyyy <d958956a6b650@gmail.com>
2022-11-19 10:03:59 +0900

    register画面の追加

---
+++ frontend/pages/register.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="register-input-container">
+        <div class="register-input-name-container">
+            <label for="register-input-name">名前</label>
+            <input id="register-input-name" type="text">
+        </div>
+        <div class="register-input-mail-container">
+            <label for="register-input-mail">e-mail</label>
+            <input id="register-input-mail" type="text">
+        </div>
+        <div class="register-input-password-container">
+            <label for="register-input-password">パスワード</label>
+            <input id="register-input-password" type="text">
+        </div>
+
+        <button>新規登録</button>
+    </div>
+
+</template>

b4a8cd29f6c4efb1e969e04d428b29c4cc7546e2
guchio33 <aokihikari3@gmail.com>
2022-11-26 21:56:39 +0900

    コントローラーの作成

--- backend/app/controllers/rooms_controller.rb
+++ backend/app/controllers/rooms_controller.rb
@@ -1,22 +1,29 @@
 class RoomsController < ApplicationController
+    #自分が取引しているDM欄
     def index
-        rooms = Room.all
+        #高順に並び替え
+        rooms = current_user
+        # rooms = Room.all
+        render json:rooms
+    end
 
+    #Roomの作成 取引開始後に起動
     def create
         @room = Room.create
         @entry1=Entry.create(room_id: @room.id,user_id: current_user.id)
         @entry2 = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id))
     end
 
-    #
+    #Roomの詳細　メッセージのやり取りなど
     def show
         @room = Room.find(params[:id])
+        #自分のid、とroom_idの値が存在するか
         if Entry.where(user_id: current_user.id,room_id: @room.id).present?
-            #
+            #ルームのメッセージを保存
             @message = @room.messages
             #新しいメッセージの作成
             @message = Message.new
-            
+
             @entries = @room.entries
         end
     end
--- backend/app/models/room.rb
+++ backend/app/models/room.rb
@@ -2,4 +2,5 @@ class Room < ApplicationRecord
   belongs_to :post
   has_many :messages, dependent: :destroy
   has_many :entries, dependent: :destroy
+  has_many :users, through: :entries
 end

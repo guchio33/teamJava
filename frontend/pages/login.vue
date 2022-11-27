a772c6966ca53269bf0cc5fff44e36b41e68f406
Ryo <kuma.2r.yousuke@gmail.com>
2022-11-28 00:01:06 +0900

    login.vueのcssを書きました

--- frontend/pages/login.vue
+++ frontend/pages/login.vue
@@ -1,15 +1,22 @@
 <template>
-    <div class="login-input-container">
-        <div class="login-input-mail-container">
-            <label for="login-input-mail">e-mail</label>
-            <input id="login-input-mail" type="email" v-model="login_email">
+    <div>
+        <img src="../images/Group_86.png">
+        <div>
+            <h1 class="login-title">ログイン</h1>
         </div>
-        <div class="login-input-password-container">
-            <label for="login-input-password">パスワード</label>
-            <input id="login-input-password" type="text" v-model="login_password">
+        <div class="login-input-container">
+            <div class="login-input-mail-container">
+                <label for="login-input-mail">e-mail</label>
+                <input id="login-input-mail" type="email" v-model="login_email">
+            </div>
+            <div class="login-input-password-container">
+                <label for="login-input-password">パスワード</label>
+                <input id="login-input-password" type="text" v-model="login_password">
+            </div>
+            <button v-on:click="loginUser">ログイン</button>
+            <p>{{ error_message }}</p>
+            <h1>suply</h1>
         </div>
-        <button v-on:click="loginUser">ログイン</button>
-        <p>{{ error_message }}</p>
     </div>
 </template>
 
@@ -66,3 +73,12 @@ const loginUser = async () => {
 
 }
 </script>
+
+
+<style>
+    .login-title{
+        background-color: #FF7F50;
+        color: #FFF;
+        text-align: center;
+    }
+</style>

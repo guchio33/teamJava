8a828f07f8b3c20b470e283cbe5cd969526a1aaf
guchio33 <aokihikari3@gmail.com>
2022-12-01 01:54:52 +0900

    取引詳細を作成しました

--- frontend/app.vue
+++ frontend/app.vue
@@ -11,3 +11,4 @@
     <NuxtPage />
   </div>
 </template>
+
--- frontend/pages/TradeList.vue
+++ frontend/pages/TradeList.vue
@@ -4,17 +4,20 @@
         <button v-on:click="showExhibit()">取引</button>
         <button v-on:click="showTransaction()">出品</button>
 
-        <div v-if="exhibitView"   v-for=" exhibit in exhibitList" :key=" exhibit.id" class="dm-container" v-on:click="moveDm(exhibit.id)">
-            <img src="../images/coffee.jpg" class="dm-container-user-icon">
-            <!-- <p>{{ dm.other_user.name}}</p> -->
-            <p>{{exhibit}}</p>
-        </div> 
+        <div v-if="exhibitView">
+            <div  v-for=" exhibit in exhibitList" :key=" exhibit.id" class="dm-container" v-on:click="movetrade(exhibit.id)">
+                <img src="../images/coffee.jpg" class="dm-container-user-icon">
+                <p>{{exhibit}}</p>
+            </div> 
+        </div>
 
-        <div v-if="transactionView" v-for=" transaction in transactionList" :key=" transaction.id" class="dm-container" v-on:click="movetrade(transaction.id)">
-            <img src="../images/coffee.jpg" class="dm-container-user-icon">
-            <!-- <p>{{ dm.other_user.name}}</p> -->
-            <p>{{transaction}}</p>
-        </div> 
+        <div v-if="transaction">
+            <div v-for=" transaction in transactionList" :key=" transaction.id" class="dm-container" v-on:click="movetrade(transaction.id)">
+                <img src="../images/coffee.jpg" class="dm-container-user-icon">
+                <!-- <p>{{ dm.other_user.name}}</p> -->
+                <p>{{transaction}}</p>
+            </div> 
+        </div>
     </div>
 </template>
 
@@ -22,16 +25,20 @@
 import Header from '../components/Header.vue';
 
 let exhibitView = true
-let transactionView =false
+let transaction = false
+
 
 //画面の切り替え
 const showExhibit=()=>{
     exhibitView = true
-    transactionView =false
+    transaction = false
+    console.log(exhibitView)
 }
 const showTransaction=()=>{
     exhibitView = false
-    transactionView = true
+    transaction = true
+    console.log("取引中")
+    console.log(exhibitView)
 }
 
 //DMリストの取得api
@@ -49,9 +56,9 @@ const transactionList=tradeLists._value.transaction
 console.log(exhibitList)
 console.log(transactionList)
 
-const moveDm=(room_id)=>{
-    console.log(room_id)
-    navigateTo({path: "/dm/"+`${room_id}`})
+const movetrade=(trade_id)=>{
+    console.log(trade_id)
+    navigateTo({path: "/tradedetail/"+`${trade_id}`})
 }
 </script>
 

9fec8f85dbf9717e2428af27ea6340b5fa988aef
guchio33 <aokihikari3@gmail.com>
2022-12-01 02:45:25 +0900

    user情報の取得

---
+++ frontend/pages/Profile.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <Header headerTitle="マイページ"/>
+        <p>{{profile.name}}</p>
+    </div>
+</template>
+
+<script setup >
+//DMリストの取得api
+const API_URL = 'http://localhost:4000'
+const id = localStorage.getItem('current_id')
+const input_message = ref('')
+
+const { data: profileDate} = await useFetch(API_URL+'/users/'+`${id}`, 
+    {headers:{
+        'access_token': localStorage.getItem('access_token'),
+        'client': localStorage.getItem('client'),
+        'expiry': localStorage.getItem('expiry'),
+        'uid': localStorage.getItem('uid')
+    }})
+const profile=profileDate.value
+console.log(profile)
+
+</script>
+
+<style lang="scss" scoped>
+$main-color: #FF7F50;
+
+</style>
